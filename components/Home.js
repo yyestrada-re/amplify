e9d1c6927b29e46d7088809bfb3e299f2877b8c1
Elaine Wu <elainewu@Elaines-MacBook-Pro-2.local>
2020-02-02 01:58:36 -0500

    backend

--- components/Home.js
+++ components/Home.js
@@ -2,9 +2,47 @@ import React, {Component} from 'react';
 import {TouchableOpacity, Image, Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
 
+import AWS from "aws-sdk";
 
 export default class Home extends React.Component {
   static headerShown = {header: false} 
+
+  componentDidMount() {
+    AWS.config.update({region: "us-east-2", credentials:{secretAccessKey: "LvtfTtrz/gSM/fXAaUh/xrqBJLvHLqAYRV3PhMU3", accessKeyId: "AKIA5GSQCVJRPQYHA7VT"}})
+    var ddb = new AWS.DynamoDB({apiVersion: "2012-08-10"})
+    ddb.listTables({}, function(err,data) {
+      if(err) {
+        console.log("Error", err.code);
+      } else {
+        console.log("Tables names are", data.TableNames);
+      }
+    });
+
+    var params = {
+      TableName: "Cart",
+    }
+
+    ddb.describeTable(params, function(err,data) {
+      if(err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.Table.KeySchema)
+      }
+    })
+
+    var params = {
+      TableName: "Fridge",
+    }
+
+    ddb.describeTable(params, function(err,data) {
+      if(err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.Table.KeySchema)
+      }
+    })
+
+  }
 
   render() {
     return (
